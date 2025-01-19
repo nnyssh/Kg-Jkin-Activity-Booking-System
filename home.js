81a9fb7ea70107adda816be13212f6daedd30190
Aishah <2023131631@student.uitm.edu.my>
2025-01-20 02:31:28 +0800

    Add files via upload

---
+++ home.js
@@ -0,0 +1,10 @@
+function logIn() {
+    alert("Log In button clicked!");
+    // Add your login functionality here
+  }
+  
+  function signUp() {
+    alert("Sign Up button clicked!");
+    // Add your signup functionality here
+  }
+  
